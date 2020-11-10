02a89f81981a715bdafdafbf3419a61a7f7371c5
zaur <1zaur1@mail.ru>
2020-11-10 08:33:08 +0300

    Подготовка к защите

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,17 @@
 const buttonSearchHotel = document.querySelector('.button-search-hotel')
 const formModal = document.querySelector('.form')
-const dateArrival =document.querySelector('#date-arrival')
-const dateDeparture =document.querySelector('#date-departure')
-const adults =document.querySelector('#adults')
-const children =document.querySelector('#children')
+const dateArrival = document.querySelector('#date-arrival')
+const dateDeparture = document.querySelector('#date-departure')
+const adults = document.querySelector('#adults')
+const children = document.querySelector('#children')
 
 let isStorageSupport = true;
-let storageAdults = "";
-let storageChildren = "";
+let storageAdults = '';
+let storageChildren = '';
 
 try {
-  storageAdults = localStorage.getItem("adults");
-  storageChildren = localStorage.getItem("children");
+  storageAdults = localStorage.getItem('adults');
+  storageChildren = localStorage.getItem('children');
 } catch (err) {
   isStorageSupport = false;
 }
@@ -22,31 +22,55 @@ buttonSearchHotel.addEventListener('click', function (evt) {
   evt.preventDefault()
   formModal.classList.toggle('fadeInDown')
   dateArrival.focus()
-  if (storageAdults){
+  if (storageAdults) {
     adults.value = storageAdults
   }
-  if (storageChildren){
+  if (storageChildren) {
     children.value = storageChildren
   }
 })
 
-formModal.addEventListener("submit", function (evt) {
+formModal.addEventListener('submit', function (evt) {
   if (!dateArrival.value || !dateDeparture.value || !adults.value || !children.value) {
     evt.preventDefault();
-    console.log("Нужно заполнить все поля формы");
+    console.log('Нужно заполнить все поля формы');
+    if (!dateArrival.value) {
+      dateArrival.classList.add('form-input-empty')
+    } else {
+      dateArrival.classList.remove('form-input-empty')
+    }
+
+    if (!dateDeparture.value) {
+      dateDeparture.classList.add('form-input-empty')
+    } else {
+      dateDeparture.classList.remove('form-input-empty')
+    }
+
+    if (!adults.value) {
+      adults.classList.add('form-input-empty')
+    } else {
+      adults.classList.remove('form-input-empty')
+    }
+
+    if (!children.value) {
+      children.classList.add('form-input-empty')
+    } else {
+      children.classList.remove('form-input-empty')
+    }
+
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("adults", adults.value);
-      localStorage.setItem("children", children.value);
+      localStorage.setItem('adults', adults.value);
+      localStorage.setItem('children', children.value);
     }
   }
 })
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (formModal.classList.contains("fadeInDown")) {
+    if (formModal.classList.contains('fadeInDown')) {
       evt.preventDefault();
-      formModal.classList.remove("fadeInDown");
+      formModal.classList.remove('fadeInDown');
     }
   }
 })
